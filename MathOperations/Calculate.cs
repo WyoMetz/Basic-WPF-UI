aa42f071b8acfa37c8564b8684411cba842c9568
Dakota Metz <metzcorp@aol.com>
2019-01-24 11:26:06 -0500

    Built the MathLib and strapped a WPF ui to it

---
+++ MathOperations/Calculate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    public class Calculate
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public bool isIncorrect { get; set; }
+
+        public int Addition()
+        {
+            return x + y;
+        }
+
+        public int Multiplication()
+        {
+            return x * y;
+        }
+
+        public string Division()
+        {
+            if (x == 0 || y == 0)
+            {
+                isIncorrect = true;
+                return ValueFailed();
+            }
+            else
+            {
+                return (x / y).ToString();
+            }
+        }
+
+        public int Subtraction()
+        {
+            return x - y;
+        }
+
+        public string ValueFailed()
+        {
+            return "Cannot Divide by Zero";
+        }
+    }
+}
---
+++ MathUi/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows;
+using MathOperations;
+
+namespace MathUi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculate calc = new Calculate();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CheckValues();
+            if (calc.isIncorrect == false)
+            {
+                Output.Text = calc.Addition().ToString();
+            }
+        }
+
+        private void Subtract_Click(object sender, RoutedEventArgs e)
+        {
+            CheckValues();
+            if (calc.isIncorrect == false)
+            {
+                Output.Text = calc.Subtraction().ToString();
+            }
+        }
+
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+            CheckValues();
+            if (calc.isIncorrect == false)
+            {
+                Output.Text = calc.Multiplication().ToString();
+            }
+        }
+
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+            CheckValues();
+            if (calc.isIncorrect == false)
+            {
+                Output.Text = calc.Division();
+            }
+        }
+
+        private void CheckValues()
+        {
+            if (!string.IsNullOrWhiteSpace(Output.Text) && calc.isIncorrect == false)
+            {
+                UpdateList();
+            }
+            calc.isIncorrect = false;
+            try
+            {
+                calc.x = int.Parse(XValue.Text);
+                calc.y = int.Parse(YValues.Text);
+            }
+            catch (Exception ex)
+            {
+                calc.isIncorrect = true;
+                Output.Text = "Please Input Numbers for Values";
+            }
+        }
+
+        private void UpdateList()
+        {
+            OtherOutputs.Items.Add(Output.Text);
+        }
+    }
+}
